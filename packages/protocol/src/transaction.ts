bb2a4ca8659819144e030ef833cf24f8f8f5ef98
bolthold <49822014+boltholds@users.noreply.github.com>
2026-07-27 15:30:26 +0300

    Allow motion plans in transactions

--- packages/protocol/src/transaction.ts
+++ packages/protocol/src/transaction.ts
@@ -6,6 +6,7 @@ import {
   bridgeVerificationExecutionSchema,
   bridgeVerificationStepSchema,
 } from "./bridge";
+import { bridgeMotionOperationSchema } from "./motion";
 import { bridgeStyleOperationSchema } from "./style";
 import { bridgeVariantOperationSchema } from "./variant";
 
@@ -21,6 +22,10 @@ export const bridgeTransactionOperationSchema = z.discriminatedUnion("type", [
   z.object({
     type: z.literal("variant"),
     operation: bridgeVariantOperationSchema,
+  }),
+  z.object({
+    type: z.literal("motion"),
+    operation: bridgeMotionOperationSchema,
   }),
 ]);
 
